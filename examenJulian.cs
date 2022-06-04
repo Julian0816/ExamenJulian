6ec374297fcb14bda8e0aeef9e16e46fc9df0d01
Julian Martinez <julian.martinez.cs@gmail.com>
2022-06-04 14:21:04 +0200

    Segunda funcion creada

--- examenJulian.cs
+++ examenJulian.cs
@@ -1,1 +1,40 @@
-examenJulian.cs
+using System;
+namespace Demo
+{
+  public class Profesor
+  {
+      public int clave_prof;
+      public string nombre;
+      public string apellido;
+      public string especialidad;
+      public string cargo;
+    {
+      public void dictarclase () {
+
+      }
+      public void calificarExamenes () {
+
+      }
+    }
+  }
+
+  public class alumno {
+      public int clave_alum;
+      public string nombre;
+      public string apellido;
+      public int num_creditos;
+
+      public void pagar () {
+
+      }
+
+      public void retirarse () {
+
+      }
+
+      public void aplazar () {
+          
+      }
+  }
+
+}
